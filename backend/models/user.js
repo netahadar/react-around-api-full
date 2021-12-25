05790ede1e2e8df37e527a76063f5c9271a3725c
Neta Hadar Itzhak <netook6@gmail.com>
2021-12-25 22:10:02 +0200

    refactor: add default values to user name, about and avatar, add error messages to validators

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,23 +6,23 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: 'Jacques Cousteau',
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: 'Explorer',
   },
   avatar: {
     type: String,
-    required: true,
     validate: {
       validator(v) {
         return /(http:\/\/|https:\/\/)[www\.]?[a-zA-z0-9.]+\/?#?/i.test(v);
       },
       message: (props) => `${props.value} is not a valid link!`,
     },
+    default: 'https://pictures.s3.yandex.net/resources/avatar_1604080799.jpg',
   },
   email: {
     type: String,
@@ -32,6 +32,7 @@ const userSchema = new mongoose.Schema({
       validator(v) {
         return /[a-z0-9\.]+@[a-z]+\.[a-z]{2,3}[\.a-z]*/i.test(v);
       },
+      message: (props) => `${props.value} is not a valid email adress!`,
     },
   },
   password: {
@@ -41,6 +42,7 @@ const userSchema = new mongoose.Schema({
       validator(v) {
         return /[a-z0-9!@#$%,&\^\*\(\)\.\+\\\{\}\-]+/i.test(v);
       },
+      message: (props) => `${props.value} is not a valid password!`,
     },
   },
 });
