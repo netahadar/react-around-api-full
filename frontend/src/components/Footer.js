5263b16892d42c0ed43fa7be503c096629b93701
Neta Hadar Itzhak <netook6@gmail.com>
2021-12-29 22:22:40 +0200

    Prepare for upload to server

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__copyrights">© 2021 Neta Hadar Itzhak</p>
-    </footer>
-  );
-}
